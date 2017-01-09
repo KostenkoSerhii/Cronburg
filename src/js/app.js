4591dcc055e3792f14b5ee4c4b0af675276d6051
Serhii <kso_132@mail.ru>
2017-01-09 17:54:19 +0400

    edit

--- src/js/app.js
+++ src/js/app.js
@@ -5,15 +5,30 @@
 //=include lib/svgxuse.min.js
 //=include lib/jquery.magnify.js
 //=include lib/jquery-ui.min.js
+//=include lib/jquery.magnific-popup.min.js
 /*sayHello();*/
 $(document).ready(function(){
 
 	$('.slider').slick({
 		infinite: false
 	});
-	$('.zoom').magnify({
-		
-	});
+	$('.zoom').magnify();
 	$('#tabs').tabs();
+	/*begin header callback*/
+	$(".header-callback").on("click", function(e){
+		e.preventDefault();
+		$(".callback-block").slideDown(200);
+	});
+	$(".callback-block__close").on("click", function(e){
+		e.preventDefault();
+		$(".callback-block").slideUp(200);
+	});
+	/*end header callback*/
 
+	/*begin advantages popup*/
+	$('.adv-popup-link').magnificPopup({
+		type: 'inline',
+		closeOnContentClick: true
+	});
+	/*end advantages popup*/
 });
